eee02deed4b6ad8698218105ca0641ba910c37a0
Kauã Biasebetti <kauamengel17@gmail.com>
2025-04-03 19:35:02 -0300

    Add project files.

---
+++ exercicio-01.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace exercicio_01.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o comprimento da caixa: ");
+            decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Digite a largura da caixa: ");
+            decimal largura = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Digite a altura da caixa: ");
+            decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+            decimal resultado = comprimento * largura * altura;
+
+            Console.WriteLine($"O volume da caixa é de {resultado}");
+            Console.ReadLine();
+        }
+    }
+}
