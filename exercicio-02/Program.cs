b41ee0fd2b8ab783c987e289e9d1a43908130d62
Kauã Biasebetti <kauamengel17@gmail.com>
2025-04-03 19:52:53 -0300

    Exercicio 02 finalizado

---
+++ exercicio-02/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace exercicio_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o raio do cilindro: ");
+            double raio = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a altura do cilindro: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            double volume = Math.PI * Math.Pow(raio, 2) * altura;
+
+            Console.WriteLine($"O volume do cilindro é: {volume:F2}");
+            Console.ReadLine();
+        }
+    }
+}
