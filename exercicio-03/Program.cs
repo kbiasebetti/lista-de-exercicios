d0db9b6356de03bf0534f89ceb0be2550213b145
Kauã Biasebetti <kauamengel17@gmail.com>
2025-04-03 20:09:31 -0300

    Exercicio 03 finalizado

---
+++ exercicio-03/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace exercicio_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a quilometragem inicial (km): ");
+            double kmInicial = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a quilometragem final (km): ");
+            double kmFinal = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a quantidade de combustível consumido (litros): ");
+            double litrosConsumidos = Convert.ToDouble(Console.ReadLine());
+
+            double distanciaPercorrida = kmFinal - kmInicial;
+            double consumoPorKm = distanciaPercorrida / litrosConsumidos;
+
+            Console.WriteLine($"\nDistância percorrida: {distanciaPercorrida} km");
+            Console.WriteLine($"Consumo médio: {consumoPorKm:F2} km/l");
+            Console.ReadLine();
+        }
+    }
+}
