dfed8952bb7183505d9607b0772ae6ea844553ee
Kauã Biasebetti <kauamengel17@gmail.com>
2025-04-03 20:15:23 -0300

    Exercício 04 finalizado

---
+++ exercicio-04/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace exercicio_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a temperatura em graus Celsius: ");
+            decimal grausCelsius = Convert.ToDecimal(Console.ReadLine());
+
+            decimal grausFare = (grausCelsius * 9 / 5) + 32;
+
+            Console.WriteLine($"\nA temperatura em Fahrenheit é: {grausFare:F2}°F");
+            Console.ReadLine();
+        }
+    }
+}
