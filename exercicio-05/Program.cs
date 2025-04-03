e0a30a0fcc9380fdf37a4946efb9801df9289fd4
Kauã Biasebetti <kauamengel17@gmail.com>
2025-04-03 20:33:27 -0300

    Exercicio 05 finalizado

---
+++ exercicio-05/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace exercicio_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o salário base do funcionário: ");
+            decimal salarioBase = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Digite o percentual de comissão (%): ");
+            decimal percentualComissao = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Digite o total das vendas realizadas (R$): ");
+            decimal totalVendas = Convert.ToDecimal(Console.ReadLine());
+
+            decimal salarioFinal = salarioBase + (totalVendas * (percentualComissao / 100));
+
+            Console.WriteLine($"\nSalário final: R$ {salarioFinal:F2}");
+            Console.ReadLine();
+        }
+    }
+}
