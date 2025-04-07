254d24dd6f1be26cc6eafddbe280181cdfad3657
Kauã Biasebetti <kauamengel17@gmail.com>
2025-04-07 20:38:09 -0300

    Exercicio 12 finalizado

---
+++ exercicio-12/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace exercicio_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Números ímpares entre 100 e 200:");
+
+            for (int i = 101; i < 200; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
