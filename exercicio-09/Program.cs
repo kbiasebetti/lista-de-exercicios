b03e16facc5e28733480b4dbf7fcd7181a5060b3
Kauã Biasebetti <kauamengel17@gmail.com>
2025-04-07 20:26:29 -0300

    Exercicio 09 finalizado

---
+++ exercicio-09/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace exercicio_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro número: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o terceiro número: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+            {
+                int valorTemporario = a;
+                a = b;
+                b = valorTemporario;
+            }
+
+            if (a > c)
+            {
+                int valorTemporario = a;
+                a = c;
+                c = valorTemporario;
+            }
+
+            if (b > c)
+            {
+                int valorTemporario = b;
+                b = c;
+                c = valorTemporario;
+            }
+
+            Console.WriteLine($"\nOrdem decrescente: {c}, {b}, {a}");
+            Console.ReadLine();
+        }
+    }
+}
