04b21a2d3f789401b36e708b58ddb0aee28e0c46
Kauã Biasebetti <kauamengel17@gmail.com>
2025-04-07 20:22:00 -0300

    Exercicio 08 finalizado

---
+++ exercicio-08/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace exercicio_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor de A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor de B: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor de C: ");
+            int C = Convert.ToInt32(Console.ReadLine());
+
+            if (A + B < C)
+            {
+                Console.WriteLine("A + B é menor que C.");
+            }
+            else
+            {
+                Console.WriteLine("A + B não é menor que C.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
