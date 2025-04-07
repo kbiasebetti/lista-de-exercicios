d7f19cfb9a87673958e8c554131320b872a7deed
Kauã Biasebetti <kauamengel17@gmail.com>
2025-04-07 20:45:28 -0300

    Exercicio 13 finalizado

---
+++ exercicio-13/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace exercicio_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número para calcular o fatorial: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            long fatorial = 1;
+            string sequencia = "";
+
+            for (int i = numero; i >= 1; i--)
+            {
+                fatorial *= i;
+
+                if (i == 1)
+                    sequencia += "1";
+                else
+                    sequencia += i + " x ";
+            }
+
+            Console.WriteLine($"\n{numero}! = {sequencia} = {fatorial}");
+            Console.ReadLine();
+        }
+    }
+}
