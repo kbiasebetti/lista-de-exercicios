d83b0e288dda3c976db405868d4c68de12c0ea55
Kauã Biasebetti <kauamengel17@gmail.com>
2025-04-07 20:35:09 -0300

    Exercicio 11 finalizado

---
+++ exercicio-11/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace exercicio_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine($"{numero} é um número PAR.");
+            }
+            else
+            {
+                Console.WriteLine($"{numero} é um número ÍMPAR.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
