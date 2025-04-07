7383833881ce17a228a7756da5f141254f82d4de
Kauã Biasebetti <kauamengel17@gmail.com>
2025-04-07 20:20:02 -0300

    Exercicio 07 finalizado

---
+++ exercicio-07/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace exercicio_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            bool ehPrimo = true;
+
+            if (numero <= 1)
+            {
+                ehPrimo = false;
+            }
+            else
+            {
+                for (int i = 2; i <= numero / 2; i++)
+                {
+                    if (numero % i == 0)
+                    {
+                        ehPrimo = false;
+                        break;
+                    }
+                }
+            }
+
+            if (ehPrimo)
+            {
+                Console.WriteLine($"{numero} é um número primo.");
+            }
+            else
+            {
+                Console.WriteLine($"{numero} não é um número primo.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
