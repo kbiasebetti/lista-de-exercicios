f0826240cda92c6fa931363333a2c61118232c54
Kauã Biasebetti <kauamengel17@gmail.com>
2025-04-07 21:09:41 -0300

    Exercicio 14 finalizado

---
+++ exercicio-14/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace exercicio_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número limite para a sequência de Fibonacci: ");
+            int limite = Convert.ToInt32(Console.ReadLine());
+
+            int a = 0, b = 1;
+
+            Console.WriteLine("Sequência de Fibonacci até " + limite + ":");
+
+            while (a <= limite)
+            {
+                Console.Write(a + " ");
+                int proximo = a + b;
+                a = b;
+                b = proximo;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
